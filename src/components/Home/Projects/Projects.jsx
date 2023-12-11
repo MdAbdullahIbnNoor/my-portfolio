7ef9b8ad72585dd9f3133c3d3f2886394a000b8d
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-12-12 03:28:28 +0600

    front page is done

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import banner01 from '../../../assets/website/forumDashboard.png'
+import banner02 from '../../../assets/website/groupPost.png'
+import banner03 from '../../../assets/website/brandAbout.png'
 
 const Projects = () => {
   return (
@@ -7,35 +10,41 @@ const Projects = () => {
       <p className='text-lg w-6/12 text-center mx-auto mb-16'>In my learning journey I have created some information based Web Application with Good Design sense. Hope you find it wonderful</p>
       <div className="grid items-center grid-cols-1 mb-24 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
         <div>
-          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Clear overview for efficient tracking</h2>
+          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Project 1: Forum Application</h2>
           <p className="mb-5 text-base text-center text-gray-600 sm:text-left md:text-lg">
-            Handle your subscriptions and transactions efficiently with the clear overview in Dashboard. Features like the smart search option allow you to quickly find any data you’re looking for.
+          This Forum application is a full-stack web platform developed using the MERN (MongoDB, Express.js, React.js, Node.js) stack. It provides a space for users to engage in conversations through posted messages. The application is designed to be responsive across various devices, ensuring a seamless user experience.
           </p>
-          <a href="#" className="w-full btn btn-dark btn-lg sm:w-auto">Learn More</a>
+          <a href="#" className="w-full btn bg-gradient-to-r to-blue-400 from-violet-600 text-white sm:w-auto">Learn More</a>
         </div>
-        <div className="w-full h-full py-48 bg-gray-200">
-          <img src="" alt="" />
+        <div className="w-full h-full bg-gray-200">
+          <img src={banner01} className="w-full h-full" />
         </div>
       </div>
       <div className="grid flex-col-reverse items-center grid-cols-1 mb-24 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
         <div className="order-none md:order-2">
-          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Decide how you integrate Payments</h2>
+          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Project 2: Online Group Study Web Application</h2>
           <p className="mb-5 text-base text-center text-gray-600 sm:text-left md:text-lg">
-            Love to code? Next to our ready-made and free plugins you can use our expansive yet simple API; decide how you integrate Payments and build advanced and reliable products yourself from scratch.
+          The Online Group Study web application is a platform that facilitates collaborative learning by allowing users to create assignments, complete them, and grade their friends' assignments. Below are the features and usage guidelines for this web application.
           </p>
-          <a href="#" className="w-full btn btn-dark btn-lg sm:w-auto">Learn More</a>
+          <a href="#" className="w-full btn bg-gradient-to-r to-blue-400 from-violet-600 text-white sm:w-auto">Learn More</a>
         </div>
-        <div className="w-full h-full py-48 bg-gray-200"></div>
+        <div className="w-full h-full bg-gray-200">
+          <img src={banner02} className="w-full h-full" />
+        </div>
       </div>
       <div className="grid items-center grid-cols-1 mb-24 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
         <div>
-          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Clear overview for efficient tracking</h2>
+          <h2 className="mb-4 text-2xl font-extrabold tracking-tight text-center text-black md:leading-tight sm:text-left md:text-4xl">Project 3: Electronic Device Store</h2>
           <p className="mb-5 text-base text-center text-gray-600 sm:text-left md:text-lg">
-            Handle your subscriptions and transactions efficiently with the clear overview in Dashboard. Features like the smart search option allow you to quickly find any data you’re looking for.
+          The Electronic Device Store is a full-stack web application for buying and exploring electronic devices. It's divided into different sections for six top electronic brands, each featuring a diverse range of products.
+
+
           </p>
-          <a href="#" className="w-full btn btn-dark btn-lg sm:w-auto">Learn More</a>
+          <a href="#" className="w-full btn bg-gradient-to-r to-blue-400 from-violet-600 text-white sm:w-auto">Learn More</a>
         </div>
-        <div className="w-full h-full py-48 bg-gray-200"></div>
+        <div className="w-full h-full bg-gray-200">
+          <img src={banner03} className="w-full h-full" />
+        </div>
       </div>
     </section>
   )
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
 
 const Root = () => {
     return (
-        <div className="container mx-auto">
+        <div>
             {/* <Navbar></Navbar> */}
-            <Outlet></Outlet>
+            <div className="container mx-auto">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     )
 }
